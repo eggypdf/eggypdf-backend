1e8cb0ba0a03b4c073ed75de5358e287db7e5988
eggypdf <eggypdf@gmail.com>
2026-09-12 20:40:56 +0400

    Register account API blueprint

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
-"""Production WSGI entrypoint for EggyPDF.
-
-Render should use `gunicorn wsgi:app` once this branch is deployed.
-"""
+"""Production WSGI entrypoint for EggyPDF."""
 from app import app
 from career_routes import career_bp
+from account_routes import account_bp
 
 app.register_blueprint(career_bp)
+app.register_blueprint(account_bp)
