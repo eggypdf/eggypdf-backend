4a4a369b908ebd8d03d43771f119e93851c12254
eggypdf <eggypdf@gmail.com>
2026-09-13 20:48:30 +0400

    Allow Authorization header for account requests

--- wsgi.py
+++ wsgi.py
@@ -5,3 +5,10 @@ from account_routes import account_bp
 
 app.register_blueprint(career_bp)
 app.register_blueprint(account_bp)
+
+
+@app.after_request
+def allow_account_authorization_header(response):
+    """Keep the shared API CORS policy compatible with Bearer-token requests."""
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    return response
