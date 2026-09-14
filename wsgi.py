3390873859ae9f1c94c9ee7a96e680891a7893e4
eggypdf <eggypdf@gmail.com>
2026-09-14 19:09:29 +0400

    Add free ATS CV content extraction
    * Add free ATS CV text extraction endpoint
    
    * Register free ATS CV extraction route
    
    * Include free ATS extraction module in Render image
    
    * Test free ATS CV extraction endpoint
    
    * Run free ATS extraction tests

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,14 @@
 """Production WSGI entrypoint for EggyPDF."""
 from app import app
 from career_routes import career_bp
+from career_free_routes import career_free_bp
 from account_routes import account_bp
 from career_security_patch import install as install_career_security
 
 install_career_security()
 
 app.register_blueprint(career_bp)
+app.register_blueprint(career_free_bp)
 app.register_blueprint(account_bp)
 
 
