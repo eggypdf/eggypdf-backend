b91411bb78db053a0f78d2ddbba30bb0c7d382df
eggypdf <eggypdf@gmail.com>
2026-09-14 19:48:34 +0400

    Install Career Pro subscription and credit system

--- wsgi.py
+++ wsgi.py
@@ -4,17 +4,20 @@ from career_routes import career_bp
 from career_free_routes import career_free_bp
 from account_routes import account_bp
 from career_security_patch import install as install_career_security
+from career_pro_system import career_system_bp, install as install_career_pro_system
 
 install_career_security()
+install_career_pro_system()
 
 app.register_blueprint(career_bp)
 app.register_blueprint(career_free_bp)
 app.register_blueprint(account_bp)
+app.register_blueprint(career_system_bp)
 
 
 def allow_account_authorization_header(response):
     """Keep the shared API CORS policy compatible with Bearer-token requests."""
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Eggy-Request-Id"
     return response
 
 
