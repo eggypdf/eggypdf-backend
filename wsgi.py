094701521ff35e350423e610f6cdca122f2c9e7a
eggypdf <eggypdf@gmail.com>
2026-09-14 19:50:24 +0400

    Apply Career Pro credit guards after blueprint registration

--- wsgi.py
+++ wsgi.py
@@ -4,15 +4,17 @@ from career_routes import career_bp
 from career_free_routes import career_free_bp
 from account_routes import account_bp
 from career_security_patch import install as install_career_security
-from career_pro_system import career_system_bp, install as install_career_pro_system
+from career_pro_system import career_system_bp
+from career_pro_runtime_patch import install as install_career_pro_runtime
 
 install_career_security()
-install_career_pro_system()
 
 app.register_blueprint(career_bp)
 app.register_blueprint(career_free_bp)
 app.register_blueprint(account_bp)
 app.register_blueprint(career_system_bp)
+
+install_career_pro_runtime(app)
 
 
 def allow_account_authorization_header(response):
