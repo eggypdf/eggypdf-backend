8b31e90ebd56f5be6eac34a0846d611f6661fb46
eggypdf <eggypdf@gmail.com>
2026-09-20 20:57:24 +0400

    Register robust Career Pro checkout session route

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,7 @@ from career_routes import career_bp
 from career_free_routes import career_free_bp
 from account_routes import account_bp
 from career_billing_routes import billing_bp
+from career_checkout_session_routes import checkout_session_bp
 from career_subscription_routes import subscription_bp
 from career_webhook_routes import webhook_bp
 from career_security_patch import install as install_career_security
@@ -15,6 +16,7 @@ app.register_blueprint(career_bp)
 app.register_blueprint(career_free_bp)
 app.register_blueprint(account_bp)
 app.register_blueprint(billing_bp)
+app.register_blueprint(checkout_session_bp)
 app.register_blueprint(subscription_bp)
 app.register_blueprint(webhook_bp)
 install_credit_enforcement(app)
