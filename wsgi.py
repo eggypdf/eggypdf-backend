26bafb80a234608b682bb1e98bde125d6de8e100
eggypdf <eggypdf@gmail.com>
2026-09-23 14:53:22 +0400

    Register Career Pro resume API

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,7 @@ from career_reconcile_routes import reconcile_bp
 from career_billing_diagnostics import billing_diagnostics_bp
 from career_subscription_routes import subscription_bp
 from career_webhook_routes import webhook_bp
+from resume_routes import resume_bp
 from career_security_patch import install as install_career_security
 from career_credit_patch import install as install_credit_enforcement
 
@@ -25,6 +26,7 @@ app.register_blueprint(reconcile_bp)
 app.register_blueprint(billing_diagnostics_bp)
 app.register_blueprint(subscription_bp)
 app.register_blueprint(webhook_bp)
+app.register_blueprint(resume_bp)
 install_credit_enforcement(app)
 
 
